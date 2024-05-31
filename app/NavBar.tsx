38e6b993acee61355e943b643c49cdb38e880260
Robert Alim <robert_alim@yahoo.com>
2024-05-31 23:36:32 +0800

    Add navigation bar (NavBar)

---
+++ app/NavBar.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import React from "react";
+import { IoIosConstruct } from "react-icons/io";
+
+const NavBar = () => {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Estimate", href: "/estimate" },
+  ];
+
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <IoIosConstruct className="w-8 h-8" />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className="text-zinc-500 hover:text-zinc-800 hover:font-semibold transition-colors"
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
